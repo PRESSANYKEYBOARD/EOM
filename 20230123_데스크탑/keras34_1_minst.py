2e281d48e34a8a19694ac032ce0eace9696b0780
PRESSANYKEYBOARD <tjchswkd@naver.com>
2023-01-23 15:50:38 +0900

    20230123 복습_데스크탑 작업

---
+++ 20230123_데스크탑/keras34_1_minst.py
@@ -0,0 +1,93 @@
+# 데스크탑에서 세팅하고 다시 복습용으로 생성했습니다. 블루스크린 나쁜 놈 ㅠㅠㅠ
+
+# CPU: intel Core i7-8700 3.20Ghz (6C12T)
+# GPU: Nvidia Geforce RTX2070 SUPER
+# RAM: Samsung DDR4-3200 16GB x2 = 32GB
+# SSD: ADATA 8200XP 1TB
+# HDD: Western Digital 1TB 7200rpm
+
+import numpy as np
+from tensorflow.keras.datasets import mnist
+
+#1. 데이터
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+print(x_train.shape, y_train.shape)                                                         # (60000, 28, 28) (60000,)
+                                                                                            # x에는 60000장의 흑백 데이터
+                                                                                            # 데이터 내용이나 순서의 영향을 받지 않는다. reshape
+print(x_test.shape, y_test.shape)                                                           # (10000, 28, 28) (10000,)
+
+x_train = x_train.reshape(60000, 28, 28, 1)
+x_test = x_test.reshape(10000, 28, 28, 1)
+
+print(x_train.shape, y_train.shape)                                                         # (60000, 28, 28, 1) (60000,)
+print(x_test.shape, y_test.shape)                                                           # (10000, 28, 28, 1) (10000,)
+
+print(np.unique(y_train, return_counts=True))                                               # (array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=uint8), array([5923, 6742, 5958, 6131, 5842, 5421, 5918, 6265, 5851, 5949],
+                                                                                            #   dtype=int64))
+                                                                    
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
+
+#2. 모델
+model = Sequential()
+model.add(Conv2D(filters=64, kernel_size=(2,2), input_shape=(28, 28, 1), 
+                 activation='relu' ))                                                       # (27, 27, 128)
+model.add(Conv2D(filters=64, kernel_size=(2,2)))                                            # (26, 26, 64)
+model.add(Conv2D(filters=64, kernel_size=(2,2)))                                            # (25, 25, 64) → flatten 40000
+model.add(Flatten())                                                                        # 40000
+model.add(Dense(32, activation='relu'))                                                     # input_shape = (40000, )
+                                                                                            # (6만, 4만)이 인풋이야.
+                                                                                            # 6만 = batch_size, 4만 = input_dim
+                                                                                
+model.add(Dense(10, activation='softmax'))                                                  # arrayr가 0부터 9까지니 10개겠죠?
+
+#3. 컴파일, 훈련    
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',
+              metrics=['acc'])
+
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+es = EarlyStopping(monitor='val_loss', patience=20, mode='min', 
+                            #   restore_best_weights=False,
+                              verbose=1)
+
+import datetime                                                                         # 데이터 타임 임포트해서 명시해준다.
+date = datetime.datetime.now()                                                          # 현재 날짜와 시간이 date로 반환된다.
+
+print(date)                                                                             # 2023-01-12 14:57:54.345489
+print(type(date))                                                                       # <class 'datetime.datetime'>
+date = date.strftime("%m%d_%H%M")                                                       # date를 str(문자형)으로 바꾼다.
+                                                                                        # 0112_1457
+print(date)
+print(type(date))                                                                       # <class 'str'>
+
+filepath = './_save/MCP/'
+filename = '{epoch:04d}-{val_loss:.4f}.hdf5'                                            # -는 연산 -가 아니라 글자이다. str형이기 때문에...
+                                                                                        # 0037-0.0048.hdf5
+
+mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1,
+                      save_best_only=True,
+                    #   filepath=path + 'MCP/keras31_ModelCheckPoint3.hdf5')
+                      filepath = filepath + 'k34_01' + date + '_' + filename
+)
+                                                                                       
+model.fit(x_train, y_train, epochs=100, batch_size=32,
+          validation_split=0.2,
+          callbacks=[es, mcp],
+          verbose=1)                                                                    # val_loss 즉, 검증할 때 손실값이 출력된다.
+                                                                                        # 기준을 잡을 때, val_loss로 기준을 잡는다.
+                                                                        
+# model.save(path + "keras31_ModelCheckPoint3_save_model.h5")
+
+#4. 평가, 예측
+results = model.evaluate(x_test, y_test)
+print('loss:', results[0])                                                              # loss 값 반환
+print('acc:', results[1])                                                               # acc 값 반환
+
+# es, mcp 적용 / val 적용
+
+"""
+loss: 0.3412693738937378
+acc: 0.9643999934196472
+"""
