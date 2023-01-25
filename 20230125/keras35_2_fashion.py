23f13ee96ef57cf74209bbe510209be489612116
PRESSANYKEYBOARD <tjchswkd@naver.com>
2023-01-25 20:21:04 +0900

    20230125 수업

---
+++ 20230125/keras35_2_fashion.py
@@ -0,0 +1,110 @@
+from tensorflow.keras.datasets import fashion_mnist
+import numpy as np
+from tensorflow.keras.datasets import mnist
+
+(x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+print(x_train.shape, y_train.shape)                                 # (60000, 28, 28) (60000,)
+                                                                    # x에는 60000장의 흑백 데이터
+                                                                    # 데이터 내용이나 순서의 영향을 받지 않는다. reshape
+print(x_test.shape, y_test.shape)                                   # (10000, 28, 28) (10000,)
+
+print(x_train[1000])
+print(y_train[1000])                                                
+
+import matplotlib.pyplot as plt
+plt.imshow(x_train[10], 'gray')
+plt.show()                                                          
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D        # Maxpool이나 Pooing이나 똑같음
+
+#2. 모델
+model = Sequential()
+model.add(Conv2D(filters=128, kernel_size=(3,3), input_shape=(28, 28, 1), 
+                 padding='same',    # vaild
+                 strides=2,
+                 activation='relu' ))                                           # (28, 28, 128)
+model.add(MaxPooling2D())                                                       # (14, 14, 128)
+                                                                                # 28에서 14로 반을 잘라버림.
+                                                                                # 연산량 없이 압축이 가능하다.
+model.add(Conv2D(filters=64, kernel_size=(2,2), 
+                 padding='same'))                                               # (26, 26, 64)
+model.add(Conv2D(filters=64, kernel_size=(2,2)))                                # (25, 25, 64) → flatten 40000
+model.add(Flatten())                                                            # 40000
+model.add(Dense(32, activation='relu'))                                         # input_shape = (40000, )
+                                                                                # (6만, 4만)이 인풋이야.
+                                                                                # 6만 = batch_size, 4만 = input_dim
+                                                                                
+model.add(Dense(10, activation='softmax'))                                      # arrayr가 0부터 9까지니 10개겠죠?
+
+model.summary()
+
+#3. 컴파일, 훈련    
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',
+              metrics=['acc'])
+
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+es = EarlyStopping(monitor='val_loss', patience=20, mode='min', 
+                            #   restore_best_weights=False,
+                              verbose=1)
+
+import datetime                                                                 # 데이터 타임 임포트해서 명시해준다.
+date = datetime.datetime.now()                                                  # 현재 날짜와 시간이 date로 반환된다.
+
+print(date)                                                                     # 2023-01-12 14:57:54.345489
+print(type(date))                                                               # <class 'datetime.datetime'>
+date = date.strftime("%m%d_%H%M")                                               # date를 str(문자형)으로 바꾼다.
+                                                                                # 0112_1457
+print(date)
+print(type(date))                                                               # <class 'str'>
+
+filepath = './_save/MCP/'
+filename = '{epoch:04d}-{val_loss:.4f}.hdf5'                                    # -는 연산 -가 아니라 글자이다. str형이기 때문에...
+                                                                                # 0037-0.0048.hdf5
+
+mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1,
+                      save_best_only=True,
+                    #   filepath=path + 'MCP/keras31_ModelCheckPoint3.hdf5')
+                      filepath = filepath + 'k35_02' + date + '_' + filename
+)
+                                                                                       
+model.fit(x_train, y_train, epochs=100, batch_size=32,
+          validation_split=0.2,
+          callbacks=[es, mcp],
+          verbose=1)                                                    # val_loss 즉, 검증할 때 손실값이 출력된다.
+                                                                        # 기준을 잡을 때, val_loss로 기준을 잡는다.
+                                                                        
+# model.save(path + "keras31_ModelCheckPoint3_save_model.h5")
+
+#4. 평가, 예측
+results = model.evaluate(x_test, y_test)
+print('loss:', results[0])                                                     # loss 값 반환
+print('acc:', results[1])                                                      # acc 값 반환
+
+# es, mcp 적용 / val 적용
+
+
+"""
+기존 성능
+loss: 0.3412693738937378
+acc: 0.9643999934196472
+
+padding 적용 후
+loss: 0.39046329259872437
+acc: 0.9668999910354614
+
+Maxpool 적용 후
+loss: 0.17730075120925903
+acc: 0.9746999740600586
+
+구글
+loss: 0.1927846521139145
+acc: 0.9732999801635742
+
+strides=3
+loss: 0.4648902416229248
+acc: 0.8572999835014343
+
+"""
---
+++ 20230125/keras39_cnn02_cailfornia.py
@@ -0,0 +1,125 @@
+# [실습]
+#1. TRAIN 0.7 이상
+#2. R2: 0.8 이상 / RMSE 사용
+
+from sklearn.datasets import fetch_california_housing
+from tensorflow.keras.models import Sequential, Model, load_model        # model은 인풋 레이어를 명시해줘야함.
+from tensorflow.keras.layers import Dense, Input, Dropout, Conv2D, Flatten, MaxPooling2D
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import numpy as np
+from sklearn.metrics import r2_score
+
+path = './_save/'
+# path = '.._save/'
+# path = 'c:/study/_save/'                                               # 셋 다 모두 동일함.
+
+#1. 데이터
+datasets = fetch_california_housing()
+x = datasets.data
+y = datasets['target']
+
+# print("최소값:", np.min(x))                                           # 0
+# print("최대값:", np.max(x))                                           # 1
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, shuffle=True,
+    random_state=333,
+    test_size=0.2
+    # stratify=y
+)
+
+scaler = MinMaxScaler()                                                 # x_train에 대한 범위의 가중치 생성
+x_train = scaler.fit_transform(x_train)                     
+x_test = scaler.transform(x_test)
+
+print(x_train.shape, x_test.shape)                                      # (16512, 8) (4128, 8)
+
+x_train = x_train.reshape(16512, 2, 2, 2)
+x_test = x_test.reshape(4128, 2, 2, 2)
+print(x_train.shape, x_test.shape)                                      # (16512, 2, 2, 2) (4128, 2, 2, 2)
+
+# 2. 모델구성(순차형)
+model = Sequential()
+model.add(Conv2D(64, (2,1), input_shape=(2, 2, 2)))
+model.add(Dense(25, activation='relu'))
+model.add(Flatten())
+model.add(Dense(15))
+model.add(Dropout(0.2))
+model.add(Dense(20))
+model.add(Dense(10))
+model.add(Dense(5))                                                     
+model.add(Dense(1, activation='linear'))
+model.summary()                                                         # 노드의 총 갯수는 drop을 해도 모두 동일하다.
+                                                                        # drop을 해도 평가할 때는 전체 노드는 다 쓴다. / 훈련시에만 적용
+
+# 2. 모델구성(함수형)
+# input1 = Input(shape=(8, ))                                           
+# Dense1 = Dense(50, activation='relu')(input1)
+# drop1 = Dropout(0.5)(Dense1)
+# Dense2 = Dense(40, activation='sigmoid')(drop1)
+# drop2 = Dropout(0.3)(Dense2)
+# Dense3 = Dense(30, activation='relu')(drop2)
+# drop3 = Dropout(0.2)(Dense3)
+# Dense4 = Dense(20, activation='linear')(Dense3)
+# output1 = Dense(1, activation='linear')(Dense4)
+# model = Model(inputs=input1, outputs=output1)                           # 시작하는 부분과 끝 부분이 어디인지 알려주는 부분
+# model.summary()
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam',     
+             metrics=['mae'])
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+es = EarlyStopping(monitor='val_loss', patience=20, mode='min',
+                            #   restore_best_weights=False,
+                              verbose=1, )
+
+import datetime                                                                 # 데이터 타임 임포트해서 명시해준다.
+date = datetime.datetime.now()                                                  # 현재 날짜와 시간이 date로 반환된다.
+
+print(date)                                                                     # 2023-01-12 14:57:54.345489
+print(type(date))                                                               # <class 'datetime.datetime'>
+date = date.strftime("%m%d_%H%M")                                               # date를 str(문자형)으로 바꾼다.
+                                                                                # 0112_1457
+print(date)
+print(type(date))                                                               # <class 'str'>
+
+filepath = './_save/MCP/'
+filename = '{epoch:04d}-{val_loss:.4f}.hdf5'                                    # -는 연산 -가 아니라 글자이다. str형이기 때문에...
+                                                                                # 0037-0.0048.hdf5
+
+mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1,
+                      save_best_only=True,
+                    #   filepath=path + 'MCP/keras31_ModelCheckPoint3.hdf5')
+                      filepath = filepath + 'k39_02' + date + '_' + filename
+)
+                                                                                       
+model.fit(x_train, y_train, epochs=100, batch_size=32,
+          validation_split=0.2,
+          callbacks=[es, mcp],
+          verbose=1)                                                    # val_loss 즉, 검증할 때 손실값이 출력된다.
+                                                                        # 기준을 잡을 때, val_loss로 기준을 잡는다.
+                                                                        
+# model.save(path + "keras31_ModelCheckPoint3_save_model.h5")
+                                                                    
+#4. 평가, 예측
+print("=============== 1. 기본 출력 ========================")
+mse, mae = model.evaluate(x_test, y_test)
+print('mse:', mse)
+
+y_predict = model.predict(x_test)
+print("y_test(원래값):", y_test)
+r2 = r2_score(y_test, y_predict)
+print("r2스코어:", r2)
+
+
+'''
+mse: 0.40441712737083435
+r2스코어: 0.682580586893458
+
+cnn result
+r2스코어: 0.6462146384151866
+
+
+'''
